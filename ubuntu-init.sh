d09aa597a949d41951f4fc5db5dcc056be151873
Jesse Tane <jesse.tane@gmail.com>
2013-01-15 00:13:13 -0500

    factored out the HEREDOC scripts

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -44,35 +44,19 @@ then
 fi
 
 
-#
-# prepend PATH to .bashrc
-#
-BIN_PATH='PATH='"$USER_HOME"'/bin:$PATH'
-if [[ -z $(grep "^${BIN_PATH}$" "$USER_HOME"/.bashrc) ]]
-then
-  echo $BIN_PATH >> "$USER_HOME"/.tempbashrc
-  cat "$USER_HOME"/.bashrc >> "$USER_HOME"/.tempbashrc
-  mv "$USER_HOME"/.tempbashrc "$USER_HOME"/.bashrc
-fi
-
-
-#
 # dirs
-#
 cd "$USER_HOME"
 mkdir -p .ssh
 mkdir -p bin
 mkdir -p lib
 mkdir -p src
 mkdir -p tmp
-mkdir -p etc/serices
+mkdir -p init
 
 
-#
 # message-of-the-day
 # remember to escape ` characters!
-# on RH, force an update by running "sudo update-motd"
-#
+# force an update by running "sudo update-motd"
 if [[ ! -e 00-header ]]
 then
   chmod a+w /etc/update-motd.d/00-header
@@ -83,9 +67,7 @@ then
 fi
 
 
-#
 # ssh
-#
 if [[ ! -e .ssh/authorized_keys || -z $(grep "$KEY" .ssh/authorized_keys) ]]
 then
   chmod 700 .ssh
@@ -94,9 +76,7 @@ then
 fi
 
 
-#
 # systemwide shell stuff
-#
 echo "--- configuring systemwide environment & shell ---"
 PROFILE=/etc/profile.d/"$USER_NAME".sh
 if [[ ! -e "$PROFILE" ]]
@@ -105,22 +85,19 @@ then
   echo 'export PS1='\""$PS1"\" >> "$PROFILE"
   echo 'export NODE_ENV='"$ENVIRONMENT" >> "$PROFILE"
   echo 'alias l="ls -alhBi --group-directories --color"' >> "$PROFILE"
+  
+  # HACK: since ubuntu ships with an intense .bashrc 
+  # that overwrites a lot of our global .profile, we re-source
+  # the global from .profile, which runs after .bashrc
+  cat /etc/passwd | while read LINE
+  do
+    HOME_DIR=$(echo "$LINE" | cut -d: -f6)
+    ". $PROFILE" >> "$HOME_DIR"/.profile
+  done
 fi
 
 
-#
-# the global "ls" alias will be masked by the one .bashrc already has
-#
-cat /etc/passwd | while read LINE
-do
-  USER_HOME=$(echo "$LINE" | cut -d: -f6)
-  [[ -e "$USER_HOME"/.bashrc ]] && sed -i 's/alias l=.*//g' "$USER_HOME"/.bashrc
-done
-
-
-#
-# some db's want this
-#
+# default max open files is kinda low
 LIMITS=/etc/security/limits.conf
 if [[ -z $(grep "app soft nofile 40000" "$LIMITS") ]]
 then
@@ -129,35 +106,8 @@ then
 fi
 
 
-#
-# use upstart to bootstrap user services on net-device-up
-#
-UPSTART_USER_JOBS='#
-# user-jobs.conf
-#
-
-description "start user jobs when networking is up"
-
-start on net-device-up
-
-script
-  cat /etc/passwd | while read LINE
-  do
-    USER=$(echo "$LINE" | cut -d: -f1)
-    USER_HOME=$(echo "$LINE" | cut -d: -f6)
-    SERVICES="$USER_HOME"/lib/
-    if [ -d "$SERVICES" ]
-    then
-      cd "$SERVICES"
-      ls | while read JOB
-      do
-        sudo -u "$USER" ./"$JOB"
-      done
-    fi
-  done
-end script
-'
-echo "$UPSTART_USER_JOBS" > /etc/init/user-jobs.conf
+# run $USER/init scripts on net-device-up
+curl -o /etc/init/upstarter.conf https://raw.github.com/jessetane/upstarter/master/upstarter.conf
 
 
 #
@@ -166,39 +116,13 @@ apt-get install build-essential -y
 apt-get install git -y
 
 
-#
-# node
-#
-NODE_INSTALLER='VERSION="$1"
-PREFIX='$"USER_HOME"'
-OS="linux"
-NODE_NAME=node-"$VERSION"-"$OS"-x64
-
-cd "$PREFIX"/lib
-
-if [[ $NODE_NAME && ! -d $NODE_NAME ]]
-then
-  echo "version $VERSION not found, attempting to download..."
-  set -e
-  wget http://nodejs.org/dist/"$VERSION"/"$NODE_NAME".tar.gz
-  tar -xvzf "$NODE_NAME".tar.gz
-  rm -rf "$NODE_NAME".tar.gz
-fi
-
-cd $NODE_NAME
-ls | while read LISTING
-do
-  [ -d "$LISTING" ] && cp -R "$LISTING" "$PREFIX"/
-done'
-echo "$NODE_INSTALLER" > bin/install-node
-chmod +x bin/install-node
-bin/install-node v0.9.6
+# install a node version manager and v0.9.6
+curl -o bin/ninstall https://raw.github.com/jessetane/ninstall/master/ninstall && chmod +x bin/ninstall
+bin/ninstall v0.9.6
 NPM=bin/npm
 
 
-#
 # chown everything in the new user's home folder and we're done
-#
 chown -R "$USER_NAME":"$USER_NAME" "$USER_HOME"
 
 
